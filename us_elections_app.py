99054afb142c1b794dd32e21a7a14e4b09fd3d8f
ritesh-suhag <ritesh_10@tamu.edu>
2021-03-31 23:02:05 -0500

    Final Edits.

--- us_elections_app.py
+++ us_elections_app.py
@@ -33,21 +33,62 @@ def home_page():
     # Problem Statement - 
     st.write("""
              ## Problem Statement
-             Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
              """)
+    st.markdown("""
+                <p style='text-align: justify;'>
+             Money has a huge impact on US presidential elections. The aim is to look into the depth of how political parties spend 
+             and get money as donations during the Presidential elections. As an analyst, we have to observe patterns, 
+             check the effectiveness of the expenditures and provide some inferences and recommendations as to where to 
+             invest the money for the next Presidential elections. 
+             </p>
+                """, unsafe_allow_html=True)
     
     # Data Collection and Pre-processing -
-    st.write(" ")
     st.write("""
              ## Data Collection and Pre-processing
-             Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
              """)
+    st.markdown("""
+                <p style='text-align: justify;'>
+             The analysis has been made on two groups of data: Donations and Expenditures. We created a dataset 
+             to analyze the Donations received by the two major political parties (i.e. Democrats and Republicans). 
+             The data from the major corporate donations have been taken into account. The corporate donation data 
+             has been segregated based on the industry sector i.e. Finance, Healthcare, Defence, law, Energy, etc. 
+             </p>
+                """, unsafe_allow_html=True)
+    st.markdown("""
+                <p style='text-align: justify;'>
+             Donation data over the years have been collected and consolidated into a single datasheet. 
+             Additionally, a state column has been created based on the location of the Corporate Headquarters. 
+             </p>
+                """, unsafe_allow_html=True)
+    st.markdown("""
+                <p style='text-align: justify;'>
+             Data associated with different political parties for each election year was collected and analyzed. 
+             At first, the features were cleaned and null entries corresponding to the state were removed. 
+             Further, the data was merged to create a dataset, where for each state, the expenditure for political 
+             parties as well as the share of vote they received for each election year is reported. We also worked on 
+             the expenditure data set, categorized the purpose of different investments made by each political party 
+             for further analysis. 
+             Once it was done, we looked into census data and pruned it to make it mergeable with the previously 
+             constructed data set. Since there are multiple races, we cut it down to three, namely: White population, 
+             Black population and Other population. In this way, data cleaning and feature engineering was done.
+ 
+             </p>
+                """, unsafe_allow_html=True)
     
     # Overview - 
     st.write("""
-             ## Overview of Approach 
-             Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+             ## Methodology
              """)
+    st.markdown("""
+                <p style='text-align: justify;'>
+                    
+                * **Feature Engineering:** After cleaning the dataset, some more features were constructed which includes population of several races as well as changing the total number of votes to percentage of votes. Further, we changed the expenditure amount in millions of dollars and population in 100,000. This helped in creating a better data which will be further used in unsupervised learning. 
+                * **Unsupervised Learning:** Once suitable features were engineered, we moved to some clustering methods. We approached the data in several ways using KNN, AgglomerativeClustering, Dendrograms and Topological Data Analysis. Since the data points are not large enough, we didn’t keep the number of clusters as a hard number. Rather, with the mapper package, we see a better picture of our clusters which brings us to the visualizations. 
+                * **Dynamic Visualization:** The graph which contains clusters is colored in a way which represents much better graphics along with information for every node. We host a website to represent different findings we have with an option to choose the party the analyst would like to know about. For each year, anyone on the website can look into how money or people of different races are related to vote shares of different parties for the past three elections as well as which institution prefers which party. Therefore, the website we have designed is very dynamic and suitable for everyone.
+
+             </p>
+                """, unsafe_allow_html=True)
     
     # Navigation - 
     st.write("")
@@ -119,20 +160,17 @@ def get_graph_inference(party):
 
 # Get Vote diff graphs - 
 def get_vote_diff_graphs(party):
-    col1, col2, col3 = st.beta_columns((1,0.1,1))
-    # Positive difference plot - 
-    with col1:
-        # Getting the Graph - 
-        HtmlFile = open(f"Expenditure_Demographics_Analysis/Graphs/Positive_Vote_{party}.html", 'r', encoding='utf-8')
-        source_code_2 = HtmlFile.read()
-        components.html(source_code_2, height=500)
     
-    # Negative difference plot - 
-    with col3:
-        # Getting the Graph - 
-        HtmlFile = open(f"Expenditure_Demographics_Analysis/Graphs/Negative_Vote_{party}.html", 'r', encoding='utf-8')
-        source_code_2 = HtmlFile.read()
-        components.html(source_code_2, height=500)
+    # Getting the 2016 Graph - 
+    HtmlFile = open(f"Expenditure_Demographics_Analysis/Graphs/Vote_Diff_{party}_2016.html", 'r', encoding='utf-8')
+    source_code_2 = HtmlFile.read()
+    components.html(source_code_2, height=400)
+    
+    st.write(" ")
+    # Getting the 2016 Graph - 
+    HtmlFile = open(f"Expenditure_Demographics_Analysis/Graphs/Vote_Diff_{party}_2020.html", 'r', encoding='utf-8')
+    source_code_2 = HtmlFile.read()
+    components.html(source_code_2, height=400)
 
 def get_overall_graphs(party):
     # Getting the Graph - 
@@ -258,7 +296,10 @@ def expenditure_analysis():
              The graphs are divided into two subgraphs where one corresponds to the states where the percentage of votes received by 
              Republicans/Democrats increased and the other subgraph where we see the states when the percentage of votes received by 
              Republicans/Democrats decreased between two presidential elections.  
-             
+              </p>""", unsafe_allow_html=True)
+              
+    st.markdown("""
+                <p style='text-align: justify;'>          
              The first important question, whether investing always helps is answered through all the graphs. In various states, 
              the Republicans lost votes despite an increase in expenditure of millions of dollars. The ROI for republicans is positive 
              (significantly larger than 0) for the states (except Utah) where they lost, which means they reduced their expenditures there. 
@@ -299,6 +340,8 @@ def expenditure_analysis():
 # ~~~~~~~~~~ Industry Donations Analysis Page -
 
 def get_state_wise_graph(party):
+    if party == "Democrats":
+        party = "Democratics"
     # Getting the Graph - 
     HtmlFile = open(f"Company_Donations_Analysis/Graphs/{party}_company_donation_statewise.html", 'r', encoding='utf-8')
     source_code_2 = HtmlFile.read()
@@ -327,14 +370,18 @@ def industry_donations_analysis():
     st.title("Industry Donation Analysis")
     
     # Desription -
-    st.write("""
-             An analysis of the corporate funding spanning across all the states in the United States of America has been analyzed for the last six presidential elections. A pattern emerging from how the corporate donations from the different sectors of the industry have been studied and inferences have been made accordingly.   
+    st.markdown("""
+                <p style='text-align: justify;'>  
+                An analysis of the corporate funding spanning across all the states in the United States of America has been analyzed for the last six presidential elections. A pattern emerging from how the corporate donations from the different sectors of the industry have been studied and inferences have been made accordingly.   
                
-             """)
+             </p>""", unsafe_allow_html=True)
     
     st.write("""
              ## Party-wise Donations 
-             
+             """)
+    # Desription -
+    st.markdown("""
+                <p style='text-align: justify;'> 
              For each of the states, the donations made to both the political parties: Republicans and Democrats have 
              been analyzed and the impact of these corporate funding have been analyzed on how they determine the final
               outcome. For this purpose, all these companies have been divided into different sectors depending on their
@@ -343,12 +390,13 @@ def industry_donations_analysis():
              
              
              For a detailed analysis of these party wise donations, an interactive plot has been shown for both the 
-            parties with the help of bubble plots."""
-             )
+            parties with the help of bubble plots.
+            </p>""", unsafe_allow_html=True)
+            
     st.write(" ")
     
     col1, col2, col3 = st.beta_columns((1,1.5,1))
-    party_selected_don = col2.selectbox("Select Party - ", ["Democratics", "Republicans"])
+    party_selected_don = col2.selectbox("Select Party - ", ["Democrats", "Republicans"])
     
     # Getting the state wise graph - 
     get_state_wise_graph(party_selected_don)
@@ -357,8 +405,10 @@ def industry_donations_analysis():
     st.write("")
     st.write("""
              ### **Plot Overview: **
-            
-            **Some intersting insight!** As we can see, in the bubble plot above, the Transportation companies (FedEx, 
+            """)
+    st.markdown("""
+                <p style='text-align: justify;'>         
+            As we can see, in the bubble plot above, the Transportation companies (FedEx, 
             UPS etc.) have stayed loyal to the Republican party which can be estimated by the massive amount of 
             donations that have been made to the party irrespective of the outcome of the elections. Similarly, 80% of 
             the Tech companies and the Labor Unions have remained loyal to the Democratic Party in the subsequent 
@@ -367,13 +417,16 @@ def industry_donations_analysis():
              in most of the cases. Another important observation that can be made in this case is that the major donors 
              of the Republican party in all of these Presidential campaigns have their headquarters located in smaller 
              states, mostly in Michigan and Wisconsin, where as most of the major donors of the Democratic party have 
-             their headquarters centered in New York and California.""")
+             their headquarters centered in New York and California.
+             </p>""", unsafe_allow_html=True)
     
     # Year-wise Donations - 
     st.write("""
              ## Year-wise Donations 
-             
-             **Some background thinking. Reason for thinking in this direction!** We further explore the Donation data 
+             """)
+    st.markdown("""
+                <p style='text-align: justify;'>
+                We further explore the Donation data 
              over the years to get some insights. We analyzed the Corporate Donation data for the last three 
              Presidential elections. All the money that has been donated by the companies during the non-election years 
              have been taken into account for the next Presidential year. For example, if we have the company wise 
@@ -381,7 +434,7 @@ def industry_donations_analysis():
              donation data received in 2016 so that we have a cumulative donation received by the Political parties 
              during the Political campaign years. 
               
-             """)
+             </p>""", unsafe_allow_html=True)
     
     # Getting the year wise graph - 
     get_year_wise_graph()
@@ -389,16 +442,21 @@ def industry_donations_analysis():
     # Inference/Result/Analysis (Insights)
     st.write("""
              ### Inference/Result/Analysis 
-             
-             **Interesting insights into the graphs** The above visualization shows a relation between the money donated 
+             """)
+    st.markdown("""
+                <p style='text-align: justify;'>
+                The above visualization shows a relation between the money donated 
               to the Republications vs. the money donated to the Democrats over the years by the Corporate Industries.  
               The size of the circle gives a relative understanding of the amount of money donated to the Political 
               parties. The x-axis gives the estimate of the money donated to the Democrats while the y-axis gives the 
               estimate of the money donated to the Republicans. Thus, more the circle is leaning towards the x-axis, more
               the money is invested onto Democrats and vice versa. The color of the circle denotes the category or the sector
                of industry into which the company belongs to. Let's look into the year-wise analysis:
-                     
-             * **2012:** Most of the donations came from Miscellaneous Business, Financial and Real Estate institutions 
+               </p>""", unsafe_allow_html=True)
+    st.markdown("""
+                <p style='text-align: justify;'>           
+                
+                * **2012:** Most of the donations came from Miscellaneous Business, Financial and Real Estate institutions 
              and Labor based industries. Las Vegas Sands is the biggest contributor from Miscellaneous Business and most
              of the money has been donated to the Republicans. The majority of the companies belonging to the labor 
              industry donated to the Democrats. The money donated by Financial institutions is scattered between both
@@ -412,27 +470,30 @@ def industry_donations_analysis():
              supported the Republicans irrespective of the election year. Similarly, the money donated by the labor 
              industries have increased but they stayed loyal to the Democrats and continued their support. 
                      
-             """)
+             </p>""", unsafe_allow_html=True)
     
     # Financial Organization -
     st.write("""
              ## Donations by Financial Organizations 
-             
-             **Some background thinking. Reason for thinking in this direction!** The Financial institutions, Insurance 
+             """)
+    st.markdown("""
+                <p style='text-align: justify;'>           
+                
+             The Financial institutions, Insurance 
              companies and the Real Estate companies made significant donations to the Political parties and their 
              contributions have swung a lot over the years. These institutions were not loyal in particular to any party
              and they mostly invested in the Political parties that they thought would likely win during that
               Presidential year. 
-              
-             """)
+             </p>""", unsafe_allow_html=True)
     
     # Get financial graph
     get_financial_organization_graph()
     
     # Getting inference -
     st.write("")
-    st.write("""
-            **Some intersting insight!** During the 2012 Presidential elections, the donations received were mostly 
+    st.markdown("""
+                <p style='text-align: justify;'> 
+            During the 2012 Presidential elections, the donations received were mostly 
             scattered between the Democrats and the Republicans. Although some of the companies like Perry Homes and 
             Hugo Enterprises donated mostly to the Republicans, majority of the donations were made to the Democrats.
             
@@ -443,7 +504,7 @@ def industry_donations_analysis():
             In 2020, most of the companies shifted their major chunks to the Democratic Party barring Citadel LLC, Ryan 
             Speciality group and Stephens Group which stayed loyal to the Republicans. Companies like Renaissance
              Technologies continued their support to the Democrats irrespective of the election year.
-            """)
+            </p>""", unsafe_allow_html=True)
             
     # Educational Organization -
     
@@ -453,11 +514,12 @@ def industry_donations_analysis():
     
     # Getting inference -
     st.write("")
-    st.write("""
+    st.markdown("""
+                <p style='text-align: justify;'> 
             Educational institutions also lent their support in the US Presidential Campaigns. In all the campaigns until
             now, the educational institutions, due to the diversity in their student population have donated unanimously
              to the Democratic party.
-            """)
+            </p>""", unsafe_allow_html=True)
             
 
 # ~~~~~~~~~~ Network Analysis Page -
